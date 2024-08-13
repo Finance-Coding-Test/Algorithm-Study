8053dcac9a226ee37d40f7785f7ba4a1e53a6d13
jymin99 <min10909@naver.com>
2024-08-13 14:02:13 +0900

    2024.08.13 TUE

--- Jaeyeon/Greedy/[Programmers]12973.py
+++ Jaeyeon/Greedy/[Programmers]12973.py
@@ -1,4 +1,20 @@
+# stack 사용 문제였던것
+def solution(s):
+    stack=[]
+    
+    for i in range(len(s)):
+        if not stack:
+            stack.append(s[i])
+        else:
+            if stack[-1]==s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
 
+    if stack:
+        return 0
+    else:
+        return 1
 # 시간초과 발생
 # def solution(s):
 #     list_s=list(s)
